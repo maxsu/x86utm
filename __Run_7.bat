00c4885152226987cc35dacbe0ebc8e6a0338b9e
Max Suica <max.suica@gmail.com>
2023-11-29 18:32:58 -0800

    Update __Run_7.bat

--- __Run_7.bat
+++ __Run_7.bat
@@ -1,14 +1,12 @@
-# Setup compiler environment
+REM Setup compiler environment
 SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\bin
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.BAT" 
 
-# Build x86utm Executable
+REM Build x86utm Executable
 cl.exe /EHsc x86utm.cpp api.c decode.c mem.c ops.c ops2.c prim_ops.c
 
-# Build Halt7 Assembly
+REM Build Halt7 Assembly
 cl  /GS- /FA /FaHalt7.asm  /c /arch:IA32 Halt7.c 
 
-# Process Halt7 Object File
-# Q: Why do we process the Object File, rather than the Assembly?
+REM Process Halt7 Object File
 x86utm Halt7.obj > Halt7out.txt
- 
